e784bd6984834386a41b478f6bc1a74d50ac277d
ssmyth-horizontal <ssmyth@horizontalintegration.com>
2020-02-07 11:55:56 -0600

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.enable('trust proxy');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client/build'));
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.end();
