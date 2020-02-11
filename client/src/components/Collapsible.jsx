e51bffa9721c8d44c6f4e6d452751e8360ba3c16
Eric Ranschau <eranschau@horizontalintegration.com>
2020-02-11 14:02:15 -0600

    NOJIRA: Replacing Font Awesome chevron character w/ Bootstrap Icon chevron character.

--- client/src/components/Collapsible.jsx
+++ client/src/components/Collapsible.jsx
@@ -35,7 +35,7 @@ class Collapsible extends React.Component {
     return (
       <div className={"collapsible" + (this.state.isActive ? ' isActive' : '')}>
         <h3 className="collapsible-headline" aria-expanded={this.props.isActive} aria-controls={this.props.id} data-toggle="collapse" data-target={"#" + this.props.id} onClick={this.handleClick}>
-          {this.props.label} <i className="fas fa-chevron-down"></i>
+          {this.props.label} <svg className="bi bi-chevron-down" width="1em" height="1em" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3.646 6.646a.5.5 0 01.708 0L10 12.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" clipRule="evenodd"/></svg>
         </h3>
         <div className={"collapse" + (this.props.isActive ? ' show' : '')} id={this.props.id}>
           <div className={"alert alert-light mt-3" + (this.props.subscriptions.length === 0 ? '' : ' d-none')}>No subscriptions are available for this category.</div>
