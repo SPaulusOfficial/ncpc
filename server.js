e5a223a7cc98d047d267a35df4a5777263492d42
Eric Ranschau <eranschau@horizontalintegration.com>
2020-02-14 12:09:52 -0600

    NOJIRA: Testing security headers in Express.

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ var app = express();
 
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-cache, no-stor');
-  res.set('Content-Security-Policy', "default-src 'self' data: *.herokuapp.com; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' *.herokuapp.com; img-src *.herokuapp.com *.sfmc-content.com; font-src *.fontawesome.com; connect-src 'self' data: *.herokuapp.com;");
+  res.set('Content-Security-Policy', "default-src 'self' data: *.herokuapp.com; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' *.herokuapp.com; style-src 'self' data: 'unsafe-inline'; img-src 'self' data: *.herokuapp.com *.sfmc-content.com; font-src 'self' data: *.fontawesome.com; connect-src 'self' data: *.herokuapp.com;");
   res.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   res.set('Strict-Transport-Security', 'max-age=200');
-  res.set('X-Content-Security-Policy', "default-src 'self' data: *.herokuapp.com; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' *.herokuapp.com; img-src *.herokuapp.com *.sfmc-content.com; font-src *.fontawesome.com; connect-src 'self' data: *.herokuapp.com;");
+  res.set('X-Content-Security-Policy', "default-src 'self' data: *.herokuapp.com; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' *.herokuapp.com; style-src 'self' data: 'unsafe-inline'; img-src 'self' data: *.herokuapp.com *.sfmc-content.com; font-src 'self' data: *.fontawesome.com; connect-src 'self' data: *.herokuapp.com;");
   res.set('X-Content-Type-Options', 'nosniff');
   res.set('X-Frame-Options', 'deny');
-  res.set('X-WebKit-CSP',  "default-src 'self' data: *.herokuapp.com; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' *.herokuapp.com; img-src *.herokuapp.com *.sfmc-content.com; font-src *.fontawesome.com; connect-src 'self' data: *.herokuapp.com;");
+  res.set('X-WebKit-CSP',  "default-src 'self' data: *.herokuapp.com; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' *.herokuapp.com; style-src 'self' data: 'unsafe-inline'; img-src 'self' data: *.herokuapp.com *.sfmc-content.com; font-src 'self' data: *.fontawesome.com; connect-src 'self' data: *.herokuapp.com;");
   res.set('X-XSS-Protection', '1; mode=block');
   next();
 });
