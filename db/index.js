54ce1cd801f9bfdb48cd06ec7ab0d8c00b321a72
ssmyth-horizontal <ssmyth@horizontalintegration.com>
2020-02-14 16:18:41 -0600

    update

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 const client = new Client({
-  //connectionString: "postgres://u8abtc187h14lj:pdd45f5983896cccea970dbea9b7c2be60a12bfbdabf6cb4ac61d9071b76da1f1@ec2-52-21-77-248.compute-1.amazonaws.com:5432/d4dpqs4699s6cg"
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  sll: true
 });
 
 client.connect();
