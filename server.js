c94497792068129fb5eee7322c89ae16f7595754
ssmyth-horizontal <ssmyth@horizontalintegration.com>
2020-02-17 21:04:54 -0600

    update

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/subscriptions', async function(req, res, next) {
 
     const avails = await db.query("SELECT *, avail.sfid as availableSubId, ncpc__categoryorder__c as catorder, c.name as campaignName, cm.sfid as campaignMemberId, c.sfid as campaignId, (SELECT sub.ncpc__Opt_In__c FROM "+schema+".ncpc__PC_Subscription__c as sub WHERE " + leadOrContact + " = '" + id + "' AND avail.sfid = sub.ncpc__Related_Subscription_Interest__c) as OptInState, (SELECT sfid as userSubId FROM "+schema+".ncpc__PC_Subscription__c as sub WHERE " + leadOrContact + " = '" + id + "' AND avail.sfid = sub.ncpc__Related_Subscription_Interest__c) as userSubId, (SELECT cat.ncpc__Display_Category_Text__c FROM "+schema+".ncpc__Category_Variant__c as cat WHERE avail.ncpc__CategoryId__c = cat.ncpc__Category__c AND "+ catLangBUClause +") as CategoryName FROM "+schema+".ncpc__PC_Available_Subscription_Interest__c as avail INNER JOIN "+schema+".ncpc__Available_Subscription_Variant__c as variant ON avail.sfid = variant.ncpc__Available_Subscription_Interest__c LEFT JOIN "+schema+".campaign c ON c.ncpc__related_subscription__c = avail.sfid LEFT JOIN "+schema+".campaignmember cm ON cm.campaignid = c.sfid WHERE avail.ncpc__Status__c = true AND avail.ncpc__Type__c = 'Subscription' AND "+ variantLangBUClause +" ORDER BY avail.ncpc__order__c");
 
-    congst groupedCampaign = groupBy.groupBySubscriptionCampaign(avails.rows, 'availableSubId');
+    const groupedCampaign = groupBy.groupBySubscriptionCampaign(avails.rows, 'availableSubId');
     const groupedAvails = groupBy.groupBySubscription(groupedCampaign, 'ncpc__display_category__c');
 
     console.log(avails.rows);
