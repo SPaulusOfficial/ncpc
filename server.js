50a69cb140dca5e654e315e4bbdc46d5bb5cc1d9
ssmyth-horizontal <ssmyth@horizontalintegration.com>
2020-02-26 20:07:01 -0600

    Updated mapped field & getProfile
    Added capabilities to pull contact value from another endpoint.

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const db = require("./db");
 const groupBy = require("./groupBy");
 const uuidv1 = require('uuid/v1');
 const schema = process.env.SCHEMA;
+const getProfile = process.env.GETPROFILE;
+const postProfile = process.env.POSTPROFILE;
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://39cd071f77f34837ad6c930c5c7fc322@sentry.io/1987793' });
 
@@ -143,24 +145,54 @@ app.get('/api/profiles', cors(corsOptions), async function(req, res, next) {
     const profile = await db.query("SELECT prof.sfid as profid, prof.ncpc__field_type__c as fieldType, prof.ncpc__editable__c as disabled, prof.ncpc__order__c as order, variant.ncpc__field_text__c as label, variant.ncpc__field_placeholder_text__c as placeholder, pOption.ncpc__order__c as optionorder, pvOption.ncpc__value__c as optionvalue, pvOption.ncpc__option__c as optionlabel, pOption.sfid as optionid, * FROM "+schema+".ncpc__pc_profile_field__c as prof INNER JOIN "+schema+".ncpc__profile_field_variant__c as variant ON prof.sfid = variant.ncpc__profile_field__c LEFT JOIN "+schema+".ncpc__pc_profile_option__c as pOption ON prof.sfid = pOption.ncpc__profile_field__c LEFT JOIN "+schema+".ncpc__profile_option_variant__c as pvOption ON pOption.sfid = pvOption.ncpc__profile_option__c AND "+vpOptionLangBUClause+" WHERE prof.ncpc__status__c = true AND "+variantLangBUClause+" ORDER BY prof.ncpc__order__c");
     var profileRows = profile.rows;
 
-    const groupedProfile = groupBy.groupByProfile(profileRows, 'ncpc__'+leadOrContact+'_mapped_field__c');
+    const groupedProfile = groupBy.groupByProfile(profileRows, 'ncpc__'+leadOrContact+'mappedfield__c');
     var profileArray = groupedProfile.map(groupedProfile => groupedProfile.mappedField).join(',');
 
-    const user = await db.query("SELECT "+profileArray+" FROM "+schema+"."+leadOrContact+" WHERE sfid = '"+id+"'");
+    // if an external endpoint needs to be called, make a post call to that service for the users detail
+    if(getProfile){
+      const user = request.post(getProfile, {
+        json: {
+          id: id,
+          object: leadOrContact,
+          fields: profileArray
+        }
+      }, (error, res, body) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+        var fieldKeys = Object.keys(body)
+        for (var i=0; i<fieldKeys.length; i++) {
+          var fieldValue = body[fieldKeys[i]];
+          var getField = groupedProfile.find(field => field.mappedField.toLowerCase() === fieldKeys[i]);
+          if(getField){
+            getField['value'] = fieldValue;
+          }
+        }
+        res.render('profile', {
+          profile: groupedProfile
+        });
+      })
+      console.log(user);
+    }else{
+      const user = await db.query("SELECT "+profileArray+" FROM "+schema+"."+leadOrContact+" WHERE sfid = '"+id+"'");
 
-    //console.log(profile.rows);
+      //console.log(profile.rows);
 
-    var fieldKeys = Object.keys(user.rows[0])
-    for (var i=0; i<fieldKeys.length; i++) {
-      var fieldValue = user.rows[0][fieldKeys[i]];
-      var getField = groupedProfile.find(field => field.mappedField.toLowerCase() === fieldKeys[i]);
-      if(getField){
-        getField['value'] = fieldValue;
+      var fieldKeys = Object.keys(user.rows[0])
+      for (var i=0; i<fieldKeys.length; i++) {
+        var fieldValue = user.rows[0][fieldKeys[i]];
+        var getField = groupedProfile.find(field => field.mappedField.toLowerCase() === fieldKeys[i]);
+        if(getField){
+          getField['value'] = fieldValue;
+        }
       }
+      res.render('profile', {
+        profile: groupedProfile
+      });
     }
-    res.render('profile', {
-      profile: groupedProfile
-    });
 
   } catch (err) {
     return next(err);
