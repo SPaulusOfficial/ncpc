8790559368af0ed231968d362e8c955e15857a59
Eric Ranschau <eranschau@horizontalintegration.com>
2020-02-28 13:50:51 -0600

    NOJIRA: Replaced local dev environment proxy to resolve issue with posting data to Heroku.

--- proxy.js
+++ proxy.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const proxy = require('express-http-proxy');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
 app.use(express.static('client/build'));
 
-app.use('/api', proxy('horizontal-ncpc-dev.herokuapp.com', {
-  proxyReqPathResolver: function (req) {
-    var parts = req.url.split('?');
-    var queryString = parts[1];
-    var updatedPath = '/api' + parts[0];
-    return updatedPath + (queryString ? '?' + queryString : '');
-  }
+app.use('/api', createProxyMiddleware({
+  changeOrigin: true,
+  headers: {
+    host: 'horizontal-ncpc-dev.herokuapp.com',
+    origin: 'horizontal-ncpc-dev.herokuapp.com'
+  },
+  target: 'http://horizontal-ncpc-dev.herokuapp.com', 
 }));
 
 app.listen(process.env.PORT || 5000, () => console.log('Express server is running on localhost:5000'));
