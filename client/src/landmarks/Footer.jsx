207537e55fbd5176ac5f2c88d9121a059c9d7259
Eric Ranschau <eranschau@horizontalintegration.com>
2020-03-09 15:06:13 -0500

    NOJIRA: Adding "target=_blank" to footer links.

--- client/src/landmarks/Footer.jsx
+++ client/src/landmarks/Footer.jsx
@@ -18,8 +18,8 @@ class Footer extends React.Component {
       <footer>
         <div className="container">
           <ul className="footer-nav list-inline">
-            <li className="list-inline-item"><a href={this.props.privacyLink.url}>{this.props.privacyLink.label}</a></li>
-            <li className="list-inline-item"><a href={this.props.termsLink.url}>{this.props.termsLink.label}</a></li>
+            <li className="list-inline-item"><a href={this.props.privacyLink.url} rel="noopener noreferrer" target="_blank">{this.props.privacyLink.label}</a></li>
+            <li className="list-inline-item"><a href={this.props.termsLink.url} rel="noopener noreferrer" target="_blank">{this.props.termsLink.label}</a></li>
           </ul>
           <p className="footer-legal">{this.props.companyName}®</p>
           <p className="footer-legal">© {this.state.today.getFullYear()} {this.props.companyName}</p>
